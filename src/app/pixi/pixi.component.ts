6a5ec01fa5056acc5782af16082477c92e9b8abd
horg4sz <horg4sz@gmail.com>
2019-12-29 17:12:20 +0100

    there is a cat! it's really the internet

--- src/app/pixi/pixi.component.ts
+++ src/app/pixi/pixi.component.ts
@@ -8,19 +8,33 @@ declare var PIXI: any;
   styleUrls: ['./pixi.component.css']
 })
 
-export class PixiComponent  implements OnInit {
+export class PixiComponent implements OnInit {
+
   title = 'pixi-test';
   @ViewChild('pixiContainer', {static: true}) pixiContainer; // this allows us to reference and load stuff into the div container
 
   public app: any; // this will be our pixi application
 
   ngOnInit() {
-
-    this.app = new PIXI.Application({width: 800, height: 600}); // this creates our pixi application
-    this.app.renderer.backgroundColor = 0x061639;
-
-
+    // Aliases
+    const Application = PIXI.Application;
+    const loader = PIXI.loader;
+    const resources = PIXI.loader.resources;
+    const Sprite = PIXI.Sprite;
+    this.app = new Application({
+      width: 256,
+      height: 256,
+      antialias: true,
+      transparent: false,
+      resolution: 1
+    }); // this creates our pixi application
     this.pixiContainer.nativeElement.appendChild(this.app.view); // this places our pixi application onto the viewable document
+
+    loader
+      .add('assets/images/cat.png')
+      .load(() => {
+        this.app.stage.addChild(new Sprite(resources['assets/images/cat.png'].texture));
+      });
   }
 
 }
